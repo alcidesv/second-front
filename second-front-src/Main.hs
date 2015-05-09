c8fc6f894623b12e36a1fea471c1b2d5826856cf
alcidesv <alcidesv@zunzun.se>
2015-05-09 19:58:47 +0200

    New relative location for example website

--- second-front-src/Main.hs
+++ second-front-src/Main.hs
@@ -1,5 +1,8 @@
-
+{-# LANGUAGE OverloadedStrings #-}
 module Main where 
+
+
+import Control.Lens (set)
 
 import SecondFront
 import SecondFront.Workers
@@ -8,10 +11,30 @@ import qualified SecondFront.Logging as L(enableConsoleLogging)
 
 import SecondTransfer (enableConsoleLogging)
 
+
+-- The "InsideConfig" contains, among other things, the definitions 
+-- of virtual hosts.
+insideConfig :: InsideConfig
+insideConfig = 
+    set  
+        virtualHosts_IC     -- What to set
+        [
+            VirtualHost
+                "www.httpdos.com:8080"
+                (fileServer
+                    (set
+                        basePath_FSC
+                        "sample-website/_site"
+                        defaultFileServerConfig
+                    )
+                )
+        ]                   -- To what
+        defaultInsideConfig -- Where: in the default config
+
 main = do 
     L.enableConsoleLogging
     enableConsoleLogging
     proxyServe 
     	defaultOutsideConfig 
-    	defaultInsideConfig 
+    	insideConfig 
     	defaultTranslationConfig
